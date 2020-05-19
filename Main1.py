a88cf3a1a35ecb9f11829f78e0a1a5e0ca635f0c
Steven Bunn <bunn.steve@gmail.com>
2020-05-18 22:39:50 -0600

    third commit

--- Main1.py
+++ Main1.py
@@ -3,19 +3,19 @@
 #  wxFBfile should be located in project directory
 #  Class created in wxFBfile must be named:  wxFBclass
 #  This template is READ ONLY,  save modified file in project directory
-##############################################################################
-
+#############################################################################
 
 import wx
 import wxFBfile
 
+
 class MainGui(wxFBfile.wxFBclass):
     def __init__(self, parent):
-        wxFBfile.wxFBclass.__init__(self,parent)
+        wxFBfile.wxFBclass.__init__(self, parent)
 
 
 app = wx.App(False)
-frame =MainGui(None)
+frame = MainGui(None)
 frame.Show(True)
 app.MainLoop()
 # this is a comment
